c87339836e5afa62fb63c898678f21b275dd4418
Dydy <210338140+Dydyy-y@users.noreply.github.com>
2026-03-01 23:54:48 +0100

    fix: ajout consigne + changement mineur

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -20,7 +20,7 @@ export abstract class BaseService<T> {
   constructor(
     endpoint: string,
     baseUrl: string = import.meta.env.VITE_API_BASE_URL ||
-      "http://localhost:3000/api",
+      "http://localhost:8080/api",
   ) {
     this.baseUrl = baseUrl;
     this.endpoint = endpoint;
